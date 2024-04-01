9cb2560543ef9e4a14ed6cc46eca6276d2f25b18
Eduardo Gallegos <gallegoseduardo414@gmail.com>
2024-04-01 19:03:33 +0000

    Initial Commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Header } from './Header'
+import { UnControlledForm } from './UnControlledForm'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <UnControlledForm/>
+    </>
+  )
+}
+
+export default App
